b06bfc25765a1a7196b92b004e544ee48066a346
David Stein <dstein131@gmail.com>
2023-05-13 16:39:53 -0400

    ok

--- queries.js
+++ queries.js
@@ -1,70 +1,19 @@
-const db = require('./db');
+const db = require('./db.js');
 
-db.query('SELECT * FROM channelname', [], (err, res) => {
-  if (err) {
-    console.error(err.stack);
-  } else {
-    console.log(res.rows);
-  }
-  db.end();
-});
+const insertLivestreamData = (data) => {
+  const queryText = 'INSERT INTO livestream_data(video_title, video_views, video_likes, video_concurrent_viewers, video_percentage) VALUES($1, $2, $3, $4, $5)';
 
-const insertVideo = async (video) => {
-    const { video_id, title, views, likes, concurrent_viewers, live_percentage, superchat_total } = video;
-    const query = `
-      INSERT INTO video (video_id, title, views, likes, concurrent_viewers, live_percentage, superchat_total)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
-    `;
-    try {
-      await db.query(query, [video_id, title, views, likes, concurrent_viewers, live_percentage, superchat_total]);
-      console.log(`Video ${video_id} inserted successfully!`);
-    } catch (err) {
-      console.error(err.stack);
-    }
-  };
-  
-  const insertSuperchat = async (superchat) => {
-    const { video_id, user_name, message, amount, timestamp } = superchat;
-    const query = `
-      INSERT INTO superchats (video_id, user_name, message, amount, timestamp)
-      VALUES ($1, $2, $3, $4, $5)
-    `;
-    try {
-      await db.query(query, [video_id, user_name, message, amount, timestamp]);
-      console.log(`Superchat from ${user_name} inserted successfully!`);
-    } catch (err) {
-      console.error(err.stack);
-    }
-  };
-  
-  const getAllVideos = async () => {
-    const query = 'SELECT * FROM video';
-    try {
-      const res = await db.query(query);
-      console.log('Fetched all videos successfully!');
-      return res.rows;
-    } catch (err) {
-      console.error(err.stack);
-    }
-  };
-
-  const getAllSuperchats = async () => {
-    const query = 'SELECT * FROM superchats';
-    try {
-      const res = await db.query(query);
-      console.log('Fetched all superchats successfully!');
-      return res.rows;
-    } catch (err) {
-      console.error(err.stack);
-    }
-  };
-  
-  // export the functions we just created so they can be used elsewhere
-    module.exports = {
-        insertVideo,
-        insertSuperchat,
-        getAllVideos,
-        getAllSuperchats
-    };
+  data.forEach(item => {
+    const values = [item.title, item.views, item.likes, item.concurrentViewers, item.livePercentage];
     
-  
+    db.query(queryText, values, (err, res) => {
+      if (err) {
+        console.log('Error inserting data: ', err.stack);
+      } else {
+        console.log('Inserted data successfully: ', res.rows[0]);
+      }
+    });
+  });
+};
+
+module.exports = insertLivestreamData;
