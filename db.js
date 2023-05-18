14e7ab5aeadcecebc4b854647dae6aabce1dc473
David Stein <dstein131@gmail.com>
2023-05-18 18:02:51 -0400

    Update app

--- db.js
+++ db.js
@@ -1,11 +1,23 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'postgres',
-  database: 'superchats',
-  host: 'localhost',
-  port: 5432,
-});
+let pool;
+if (process.env.DATABASE_URL) {
+  // Heroku connection
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  // Local connection
+  pool = new Pool({
+    user: 'postgres',
+    database: 'superchats',
+    host: 'localhost',
+    port: 5432,
+  });
+}
 
 module.exports = {
   query: (text, params, callback) => {
